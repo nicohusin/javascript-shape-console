397742bb0b1bd802423a5c95cd198ab06aa0f84b
nicohusin <nicohusin@gmail.com>
2019-09-04 11:49:58 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const rectangleWidth = 100
+const rectangleLength = 20
+
+const rectangleArea = rectangleWidth * rectangleLength
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`)
+
+const phi = 22/7
+const diameter = 14
+
+const circleArea = 0.25 * phi * diameter **2
+
+console.log(`${circleArea} = 0.25 * ${phi} * ${diameter} **2`)
